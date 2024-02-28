25b163cd91db52883bd46d5f4cac3be28ced14a4
andjared <andja.velimirovic@gmail.com>
2024-02-28 11:21:01 +0100

    Created script to fill tables with test examples

--- product-service/src/data.mjs
+++ product-service/src/data.mjs
@@ -36,3 +36,30 @@ export const products = [
 		title: 'Book six',
 	},
 ];
+
+export const stocks = [
+	{
+		product_id: '7567ec4b-b10c-48c5-9345-fc73c48a80aa',
+		count: 1,
+	},
+	{
+		product_id: '7567ec4b-b10c-48c5-9345-fc73c48a80a1',
+		count: 2,
+	},
+	{
+		product_id: '7567ec4b-b10c-48c5-9345-fc73c48a80a3',
+		count: 3,
+	},
+	{
+		product_id: '7567ec4b-b10c-48c5-9345-fc73348a80a1',
+		count: 4,
+	},
+	{
+		product_id: '7567ec4b-b10c-48c5-9445-fc73c48a80a2',
+		count: 5,
+	},
+	{
+		product_id: '7567ec4b-b10c-45c5-9345-fc73c48a80a1',
+		count: 6,
+	},
+];
