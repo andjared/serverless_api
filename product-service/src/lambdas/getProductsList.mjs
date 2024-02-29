acc8dbe5e2b180fe48a18f9e7d261503155a4c57
andjared <andja.velimirovic@gmail.com>
2024-02-29 12:42:45 +0100

    Scan products and stocks table, join results

--- product-service/src/lambdas/getProductsList.mjs
+++ product-service/src/lambdas/getProductsList.mjs
@@ -1,9 +1,46 @@
-import { products } from '../data.mjs';
+import AWS from 'aws-sdk';
+
+const client = new AWS.DynamoDB.DocumentClient();
+
+const scanProductsTable = async () => {
+	try {
+		const scanResults = await client
+			.scan({
+				TableName: process.env.PRODUCTS_TABLE,
+			})
+			.promise();
+
+		return scanResults.Items;
+	} catch (err) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ message: 'Error scanning db' }),
+		};
+	}
+};
+
+const scanStocksTable = async () => {
+	const scanResults = await client
+		.scan({
+			TableName: process.env.STOCKS_TABLE,
+		})
+		.promise();
+
+	return scanResults.Items;
+};
 
 export async function getProductsList() {
-	console.log('products', products);
+	const products = await scanProductsTable();
+	const stocks = await scanStocksTable();
+
+	const joined = products.map((product) => {
+		const stockInfo = stocks.find((stock) => stock.product_id === product.id);
+
+		return { ...product, count: stockInfo.count || 0 };
+	});
+
 	return {
 		statusCode: 200,
-		body: JSON.stringify(products),
+		body: JSON.stringify(joined),
 	};
 }
