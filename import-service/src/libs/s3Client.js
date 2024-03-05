70055a7ee6f5b349e93a19e29f33273173ff8ed5
andjared <andja.velimirovic@gmail.com>
2024-03-05 11:19:59 +0100

    ImportProductsFile lambda return signedUrl

--- import-service/src/libs/s3Client.js
+++ import-service/src/libs/s3Client.js
@@ -1,6 +1,5 @@
-import { S3Client } from '@aws-sdk/client-s3”;';
+import { S3Client } from '@aws-sdk/client-s3';
 
 const REGION = process.env.REGION;
 
-const s3Client = new S3Client({ region: REGION });
-export { s3Client };
+export const s3client = new S3Client({ region: REGION });
