0a575374c9e018003fafc4bf9991d544eeed3e3b
andjared <andja.velimirovic@gmail.com>
2024-03-07 09:39:54 +0100

    Configure the SQS to trigger lambda catalogBatchProcess with 5 messages at once via batchSize property

--- product-service/handler.mjs
+++ product-service/handler.mjs
@@ -1,3 +1,4 @@
+export { catalogBatchProcess } from './src/lambdas/catalogBatchProcess.mjs';
 export { createProduct } from './src/lambdas/createProduct.mjs';
 export { getProductById } from './src/lambdas/getProductById.mjs';
 export { getProductsList } from './src/lambdas/getProductsList.mjs';
