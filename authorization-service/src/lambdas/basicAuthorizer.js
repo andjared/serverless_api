8b8f85ea67f92353f2f3d10e318f0bbde48b82bd
andjared <andja.velimirovic@gmail.com>
2024-03-09 19:25:09 +0100

    Decode credentials

--- authorization-service/src/lambdas/basicAuthorizer.js
+++ authorization-service/src/lambdas/basicAuthorizer.js
@@ -1,6 +1,13 @@
 export const basicAuthorizer = (event) => {
 	const { headers } = event;
-	console.log(event, 'event');
-	console.log(process.env.ANDJARED, 'password');
+	const credentials = headers.Authorization.split(' ')[1];
+	const decoded = Buffer.from(credentials, 'base64').toString();
+	console.log(credentials, 'credentials');
+	console.log(decoded, 'decoded');
+
+	const [username, password] = decoded.toString('utf-8').split(':');
+
+	console.log(` username: "${username}" password: "${password}"`);
+	console.log(process.env[username], 'password');
 	console.log(headers.Authorization, 'header auth');
 };
