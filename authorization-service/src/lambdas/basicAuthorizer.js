7c0509de4498ddec7d79be1e6822f6ef03a7d142
andjared <andja.velimirovic@gmail.com>
2024-03-11 12:19:32 +0100

    Fix typo

--- authorization-service/src/lambdas/basicAuthorizer.js
+++ authorization-service/src/lambdas/basicAuthorizer.js
@@ -3,7 +3,7 @@ export const basicAuthorizer = async (event) => {
 	try {
 		const { headers } = event;
 
-		const encoded = headers.Authorization.split(' ')[1];
+		const encoded = headers.authorization.split(' ')[1];
 		const decoded = Buffer.from(encoded, 'base64').toString('utf-8');
 		const [username, password] = decoded.split(':');
 
